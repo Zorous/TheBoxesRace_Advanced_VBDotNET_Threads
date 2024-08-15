ee889ab82819e220ab3caef66e6daa8a231f439c
OussamaBelhadi <oussama.belhadi@ttline.com>
2024-08-15 15:03:51 +0200

    Creating Threads and Managing them

---
+++ Form1.vb
@@ -0,0 +1,69 @@
+﻿Public Class Form1
+    Dim rand As New Random
+    Dim maxMove As Integer = 5
+    Dim maxSleep As Integer = 10
+
+    Dim t1 As System.Threading.Thread
+    Dim t2 As System.Threading.Thread
+
+    Private Sub btn_start_Click(sender As Object, e As EventArgs) Handles btn_start.Click
+        'to run the two threads at once
+        'the AdressOf property indicates which procedure I want to be running in that Thread
+        t1 = New System.Threading.Thread(AddressOf Redbox)
+        t2 = New System.Threading.Thread(AddressOf Bluebox)
+
+        t1.Start()
+        t2.Start()
+
+        'Call Redbox()
+        'Call Bluebox()
+    End Sub
+
+
+    Sub Redbox()
+        Dim G As Graphics
+        G = Me.CreateGraphics
+
+        Dim bRed As Brush
+        Dim bGrey As Brush
+        Dim xR As Integer
+        xR = 100
+        bRed = New SolidBrush(Color.Red)
+        bGrey = New SolidBrush(Me.BackColor)
+
+        Do While xR < 1150
+            G.FillRectangle(bRed, xR, 100, 50, 50)
+            Threading.Thread.Sleep(rand.Next(maxSleep))
+            G.FillRectangle(bGrey, xR, 100, 50, 50)
+            xR = xR + rand.Next(maxMove)
+
+        Loop
+        G.FillRectangle(bRed, xR, 100, 50, 50)
+
+    End Sub
+
+
+
+    Sub Bluebox()
+        Dim G As Graphics
+        G = Me.CreateGraphics
+
+        Dim bBlue As Brush
+        Dim bGrey As Brush
+        Dim xB As Integer
+        xB = 100
+        bBlue = New SolidBrush(Color.Blue)
+        bGrey = New SolidBrush(Me.BackColor)
+
+        Do While xB < 1150
+            G.FillRectangle(bBlue, xB, 200, 50, 50)
+            Threading.Thread.Sleep(rand.Next(maxSleep))
+            G.FillRectangle(bGrey, xB, 200, 50, 50)
+            xB = xB + rand.Next(maxMove)
+
+        Loop
+        G.FillRectangle(bBlue, xB, 200, 50, 50)
+
+    End Sub
+
+End Class
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("TheBoxesRace_Advanced_VBDotNET_Threads")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("TT-Line GmbH Co. KG")>
+<Assembly: AssemblyProduct("TheBoxesRace_Advanced_VBDotNET_Threads")>
+<Assembly: AssemblyCopyright("Copyright © TT-Line GmbH Co. KG 2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("5a85012c-8155-42d1-9054-fcdf19f8edd2")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
